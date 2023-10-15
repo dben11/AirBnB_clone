6a05e388e8c0613e39c9601bb2659c2c13a75417
CodeX1991 <oghenewonaholy@gmail.com>
2023-10-15 16:34:39 -0700

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,12 +10,21 @@ import models
 class BaseModel:
     """A class that defines all common attr/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Instantiation of a class attribute"""
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        if kwargs:
+            del kwargs["__class__"]
+            for key, val in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    dtime = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, val)
+                else:
+                    setattr(self, key, val)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.update_at = datetime.now()
 
     def __str__(self):
         """String representation of the output in dict format"""
