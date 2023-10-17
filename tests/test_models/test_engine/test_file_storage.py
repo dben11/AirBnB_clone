dfe65e14c2a1cb98a30ab6ce613799ede0a91b09
CodeX1991 <oghenewonaholy@gmail.com>
2023-10-17 07:45:22 -0700

    Add File storage test

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Contains the TestFileStoragr classes"""
+
+
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+    """The test suit"""
+
+    def setUp():
+        """Set up the doc test"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
