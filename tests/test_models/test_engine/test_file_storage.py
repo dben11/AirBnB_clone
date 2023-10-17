e02489e0d5350b2fd95e9b84cecf8511ecec3ce9
CodeX1991 <oghenewonaholy@gmail.com>
2023-10-17 08:00:47 -0700

    Update the test suit

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Test suits for File storage"""
+
+
+import os
+import models
+import unittest
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestFileStorage(unittest.TestCase):
+
+    def setUp():
+        """Set up the class"""
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
