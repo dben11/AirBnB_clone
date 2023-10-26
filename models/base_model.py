04986c36a150f28f70f0faceb7ccfbd062ea28a4
CodeX1991 <oghenewonaholy@gmail.com>
2023-10-26 15:32:25 -0700

    Updating files

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,9 @@
 import uuid
 from datetime import datetime
 import models
+from os import getenv
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
 
 
 class BaseModel:
@@ -14,17 +17,29 @@ class BaseModel:
         """Instantiation of a class attribute"""
 
         if kwargs:
-            del kwargs["__class__"]
             for key, val in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    dtime = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, val)
-                else:
-                    setattr(self, key, val)
+
+            if kwargs.get("created_at", None)\
+                    and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+
+            if kwargs.get("updated_at", None)\
+                    and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.update_at = datetime.now()
+            self.created_at = datetime.utcnow()
+            self.update_at = datetime.utcnow()
 
     def __str__(self):
         """String representation of the output in dict format"""
@@ -35,21 +50,33 @@ class BaseModel:
     def save(self):
         """Updates the public instance attr updated_at"""
 
-        self.updated_at = datetime.now()
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
+        models.storage.save()
 
-    def to_dict(self):
+    def to_dict(self, save_fs=None):
         """Returns a dict containing key/value of __dict__ of the instance"""
 
-        dict_format = {}
+        dict_format = self.__dict__.copy()
+        if "created_at" in dict_format:
+            dict_format["created_at"] =\
+                    dict_format["created_at"].strftime(time)
+
+        if "updated_at" in dict_format:
+            dict_format["updated_at"] =\
+                    dict_format["updated_at"].strftime(time)
 
         dict_format["__class__"] = self.__class__.__name__
-        for key, val in self.__dict__.items():
-            """get the val which are of datetime object type"""
 
-            if isinstance(val, datetime):
-                dict_format[key] = val.isoformat()
+        if "_sa_instance_state" in dict_format:
+            del dict_format["_sa_instance_state"]
 
-            else:
-                dict_format[key] = val
+        if save_fs is None:
+            if "password" in dict_format:
+                del dict_format["password"]
 
         return dict_format
+
+    def delete(self):
+        """delete the current instance from the storage"""
+        models.storage.delete(self)
