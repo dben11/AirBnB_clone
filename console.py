c7569e02d8a0e30652ed32d7fcf07b5e4dc06eb4
CodeX1991 <oghenewonaholy@gmail.com>
2023-10-27 12:26:44 -0700

    Add a program called console.py that contains the entry point of the command interpreter :

--- console.py
+++ console.py
@@ -1,7 +1,47 @@
 #!/usr/bin/python3
 """The console module"""
 
+import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     """HBNB console"""
-    prompt = '(hbnb)'
+    prompt = '(hbnb) '
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
+
+    def emptyline(self):
+        """ overwriting the emptyline method """
+        return False
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    """def _key_value_parser(self, args):
+        creates a dictionary from a list of strings
+        new_dict = {}
+        for arg in args:
+            if "=" in arg:
+                kvp = arg.split('=', 1)
+                key = kvp[0]
+                value = kvp[1]
+                if value[0] == value[-1] == '"':
+                    value = shlex.split(value)[0].replace('_', ' ')
+                else:
+                    try:
+                        value = int(value)
+                    except:
+                        try:
+                            value = float(value)
+                        except:
+                            continue
+                new_dict[key] = value
+        return new_dict
+    """
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
